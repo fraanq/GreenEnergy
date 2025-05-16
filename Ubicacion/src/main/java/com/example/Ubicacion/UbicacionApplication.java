66e9dbddf9b622ff90b661e321f26fec63104419
Gerald1023 <ger.santana@duocuc.cl>
2025-05-16 09:43:06 -0400

    agegue mis 4 microservicios

---
+++ Ubicacion/src/main/java/com/example/Ubicacion/UbicacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ubicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbicacionApplication.class, args);
+	}
+
+}
