c745e7a21f22383621bd5a5c2123098d846509c6
Gerald1023 <ger.santana@duocuc.cl>
2025-05-16 09:45:20 -0400

    dsgfgfg

--- Usuarios/src/main/java/com/example/Usuarios/UsuariosApplication.java
+++ Usuarios/src/main/java/com/example/Usuarios/UsuariosApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UsuariosApplication {
 
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UsuariosApplication.class, args);
 	}
