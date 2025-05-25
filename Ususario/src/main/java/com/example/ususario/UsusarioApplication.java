6982ac799a23a66c030b0bc8c0f08435476feeca
Gerald1023 <ger.santana@duocuc.cl>
2025-05-25 19:03:36 -0400

    Add files via upload

---
+++ Ususario/src/main/java/com/example/ususario/UsusarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.ususario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsusarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsusarioApplication.class, args);
+	}
+
+}
---
+++ Ususario/src/test/java/com/example/ususario/UsusarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ususario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsusarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
