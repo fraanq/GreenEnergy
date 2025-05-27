213d3bee18891debd3ef1d9a5354f54ba49d0d40
samuel__ignacio <sa.villanueva@duocuc.cl>
2025-05-27 19:53:32 -0400

    prueba

--- Contrataciones/src/main/java/com/example/Contrataciones/WebClient/ServicoClient.java
+++ Contrataciones/src/main/java/com/example/Contrataciones/WebClient/ServicoClient.java
@@ -27,4 +27,6 @@ public class ServicoClient {
                 .bodyToMono(Map.class)
                 .block();
     }
+
+    
 }
--- Contrataciones/src/main/java/com/example/Contrataciones/WebClient/UsuarioClient.java
+++ Contrataciones/src/main/java/com/example/Contrataciones/WebClient/UsuarioClient.java
@@ -1,22 +1,33 @@
 package com.example.Contrataciones.WebClient;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @Component
 public class UsuarioClient {
 
     private final WebClient webClient;
 
-    public UsuarioClient(WebClient.Builder builder) {
-        this.webClient = builder.baseUrl("http://localhost:8081/api/v1/usuarios").build();
+    public UsuarioClient(@Value("${usuario-service.url}") String usuarioServiceUrl) {
+        this.webClient = WebClient.builder().baseUrl(usuarioServiceUrl).build();
     }
 
-    public UsuarioClient obtenerUsuarioPorId(Long id) {
+     // metodo para realizar la consulta al microservicio de servicio
+    public Map<String, Object> getUsuarioyId(Long id) {
         return webClient.get()
-            .uri("/{id}", id)
-            .retrieve()
-            .bodyToMono(UsuarioClient.class)
-            .block();
+                .uri("/{id}", id) // Usa esto exactamente
+                .retrieve()
+                .onStatus(status -> status.is4xxClientError(),
+                        response -> response.bodyToMono(String.class)
+                                .flatMap(body -> Mono.error(new RuntimeException("Servicio no encontrado: " + body))))
+                .bodyToMono(Map.class)
+                .block();
     }
+
+    
 }
